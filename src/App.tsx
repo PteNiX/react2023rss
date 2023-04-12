82d8b908a6c5fffa3e3000f6e62e6bcce36effe4
Aliaksandr Ptsitsa <ptenix@gmail.com>
2023-04-12 20:09:05 +0300

    feat: create empty pages, try to build routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
+/* import { useState } from "react"; */
+/* import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg"; */
 import "./App.css";
-import Fcomponent from "./Fcomponent";
-import Ccomponent from "./Ccomponent";
-import Acomponent from "./Acomponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+/* import Fcomponent from "./components/Fcomponent";
+import Ccomponent from "./components/Ccomponent";
+import Acomponent from "./components/Acomponent"; */
+import Home from "./pages/Home";
+import AboutUs from "./pages/AboutUs";
+
+import Page404 from "./pages/Page404";
+/* import { AboutUs } from "./pages/AboutUs";
+import { Page404 } from "./pages/Page404"; */
 
 function App() {
-  const [count, setCount] = useState(0);
+  /*   const [count, setCount] = useState(0); */
 
   return (
     <div className="App">
-      <Fcomponent />
+      {/*       <Fcomponent />
       <Ccomponent />
-      <Acomponent />
+      <Acomponent /> */}
 
-      <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/*       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -39,7 +55,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </div>
   );
 }
