d64950122c72184240357a6a85451c8a09680a62
Aliaksandr Ptsitsa <ptenix@gmail.com>
2023-04-25 12:56:04 +0300

    feat: add input in local storage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,14 @@ export const Home = () => {
   return (
     <div>
       Home
-      <input type="text" name="name"></input>
+      <input
+        type="text"
+        name="name"
+        onChange={(event) => {
+          console.log(event.target.value);
+          localStorage.setItem("input", event.target.value);
+        }}
+      ></input>
     </div>
   );
 };
